5b703e0915d051fc9f653c82ea6a8e8a173eac26
Ângelo Castro <42939130+AngeloCastro9@users.noreply.github.com>
2019-12-10 12:36:59 -0300

    Start Project

---
+++ Fortes_Scripts/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Diagnostics;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System;
+using System.IO;
+
+namespace Fortes_Scripts
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            String caminho = System.Environment.CurrentDirectory.ToString() + "\\Teste.ini"; //Qualquer caminho
+            if (!System.IO.File.Exists(caminho))
+                System.IO.File.Create(caminho).Close();
+            StreamWriter sw = new StreamWriter(caminho);
+            sw.WriteLine("[Startup]" + "\n" +
+               "ProgramFolder = A:\\Exe\\AC" + "\n" +
+               "DataFolder = D:\\" + DataFolder.Text + "\\" + Databasefile.Text + "AC" + "\n)");
+            sw.Close();
+        }
+    }
+}
