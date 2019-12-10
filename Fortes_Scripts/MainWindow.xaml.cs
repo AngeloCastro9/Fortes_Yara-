88d2f947dc23a072cee265ab3d927476bb54323d
Ângelo Castro <42939130+AngeloCastro9@users.noreply.github.com>
2019-12-10 13:28:23 -0300

    Evol

--- Fortes_Scripts/MainWindow.xaml.cs
+++ Fortes_Scripts/MainWindow.xaml.cs
@@ -35,14 +35,40 @@ namespace Fortes_Scripts
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            String caminho = System.Environment.CurrentDirectory.ToString() + "\\Teste.ini"; //Qualquer caminho
-            if (!System.IO.File.Exists(caminho))
-                System.IO.File.Create(caminho).Close();
-            StreamWriter sw = new StreamWriter(caminho);
-            sw.WriteLine("[Startup]" + "\n" +
-               "ProgramFolder = A:\\Exe\\AC" + "\n" +
-               "DataFolder = D:\\" + DataFolder.Text + "\\" + Databasefile.Text + "_AC" + "\n)");
-            sw.Close();
+            if (ClientName.Text != "")
+            {
+                //A:\CloudUp\CloudUpCmd
+                string folder = @"C:\" + ClientName.Text; //nome do diretorio a ser criado
+                if (!Directory.Exists(folder))
+                {
+                    Directory.CreateDirectory(folder);
+                }
+
+                String caminho = folder + "\\" + ClientName.Text + ".ini"; //Qualquer caminho
+                if (!System.IO.File.Exists(caminho))
+                    System.IO.File.Create(caminho).Close();
+                StreamWriter sw = new StreamWriter(caminho);
+                sw.WriteLine("[Startup]" + "\n" +
+                   "ProgramFolder = A:\\Exe\\AC" + "\n" +
+                   "DataFolder = D:\\" + DataFolder.Text + "\\" + Databasefile.Text + "_AC" + "\n" +
+                   "DatabaseFile = 127.0.0.1:" + Databasefile.Text + "_AC" + "\n" +
+                   "DriverName = MSSQL" + "\n" +
+                   "UserName = " + Username.Text + ".sql" + "\n" +
+                   "Password = " + Password.Text + "\n" +
+                   "\n" +
+                   "[Settings]" + "\n" +
+                   "ContinueUpdateAfterCrashRecovery = True" + "\n" +
+                   "AllowOldBackupsRestoration = False" + "\n" +
+                   "SkipWarnings = True" + "\n" +
+                   "ByPassInUseCheck = True" + "\n" +
+                   "RetryOnDatabaseInUse = False" + "\n" +
+                   "\n" +
+                   "[Backup]" + "\n" +
+                   "SkipBackupDatabase = True ");
+                sw.Close();
+            } else {
+                MessageBox.Show("Nome do cliente não foi informado");
+            }
         }
     }
 }
